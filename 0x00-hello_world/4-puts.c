83e344956b73457cbe71dbf6c0a38f03d4946e10
Hershewajue <amedujosepho@gmail.com>
2023-03-16 21:40:43 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main(void){
-puts("Programming is like building a multilingual puzzle");
-return 0;
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
