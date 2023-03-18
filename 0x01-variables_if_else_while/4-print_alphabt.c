8b18e92079de31608beba33381a49ecc5c564b70
Hershewajue <amedujosepho@gmail.com>
2023-03-18 13:43:09 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alp = 'a';
+while(alp <= 'z'){
+if (alp == 'e' || alp == 'q'){
+    alp++;
+}
+putchar(alp);
+alp++;
+}
+putchar('\n');
+return (0);
+}
