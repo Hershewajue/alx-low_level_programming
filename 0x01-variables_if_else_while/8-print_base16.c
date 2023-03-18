24f5d2f050d5efbef169beeac9608c4e94ececd7
Hershewajue <amedujosepho@gmail.com>
2023-03-18 14:50:55 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alp = 'a';
+char i;
+for(i = '0'; i <= '9'; i++)
+{
+putchar(i);
+}
+while (alp <= 'f')
+{
+putchar(alp);
+alp++;
+}
+putchar('\n');
+return (0);
+}
