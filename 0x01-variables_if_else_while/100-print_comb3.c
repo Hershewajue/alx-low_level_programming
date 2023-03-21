45e56c97d307dd1b984a760a09b4363d0ad035fd
Hershewajue <amedujosepho@gmail.com>
2023-03-21 14:16:26 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Prints all possible combinations of double-digit numbers.
+ * Numbers must be separated by a comma and space.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j;
+for (i = 48; i < 57; i++)
+{
+for (j = 49; j < 58; j++)
+{
+if (j > i)
+{
+putchar(i); /* print current number */
+putchar(j);
+if (i != 56 || j!= 57) /* check if last number */
+{
+putchar(','); /* print comma and space */
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
