e59400893fce9dee586c6a4eae9a6311c2f67129
Hershewajue <amedujosepho@gmail.com>
2023-03-21 15:09:42 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,9 +6,12 @@
  */
 
 int main(void)
+
 {
 	int i, j;
+
 	for (i = 0; i < 100; i++)
+
 	{
 		for (j = 0; j < 100; j++)
 		{
@@ -26,7 +29,11 @@ int main(void)
 				}
 			}
 		}
+
 	}
+
 	putchar('\n');
+
 	return (0);
+
 }
