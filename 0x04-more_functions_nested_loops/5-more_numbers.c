d2fa314e5dc64adddbcde708efd4263809bb12f2
Hershewajue <amedujosepho@gmail.com>
2023-03-24 16:54:03 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,11 +9,11 @@ for (x = 0; x < 10; x++)
 {
 for (y = 0; y <= 14; y++)
 {
-if (y > 9)
+if (y > 9) /* if the number has 2 digits */
 {
-_putchar(y / 10 + '0');
+_putchar(y / 10 + '0'); /* print the first digit */
 }
-_putchar(y % 10 + '0');
+_putchar(y % 10 + '0'); /* print the last digit */
 }
 _putchar('\n');
 }
