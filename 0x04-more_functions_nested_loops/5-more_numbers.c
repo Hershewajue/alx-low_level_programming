b0f756ddc1b61e8e76a72e568bae3a13c439447f
Hershewajue <amedujosepho@gmail.com>
2023-03-24 16:52:43 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
-* Write a function that prints 10 times the numbers, from 0 to 14, followed by a new line.
+* more_numbers - a function that prints 10 times the numbers, from 0 to 14, followed by a new line.
 */
 void more_numbers(void)
 {
 int x, y;
 for (x = 0; x < 10; x++)
 {
-for (y = 0; y < 15; y++)
+for (y = 0; y <= 14; y++)
 {
 if (y > 9)
 {
