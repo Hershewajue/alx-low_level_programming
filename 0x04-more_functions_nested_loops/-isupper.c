74e3bdcebabb23dbd95562109a54ab3371b55fbc
Hershewajue <amedujosepho@gmail.com>
2023-03-24 15:19:45 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_isupper - this function checks upercase alphabets
+*@c: an integer
+*Return: success
+*/
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
