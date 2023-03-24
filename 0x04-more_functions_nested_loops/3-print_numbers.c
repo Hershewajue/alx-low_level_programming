74fcaf988db2c712858f4ce9d178d23b9ea7df5a
Hershewajue <amedujosepho@gmail.com>
2023-03-24 16:16:07 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_numbers - function to print numbers from 0 - 9
+* @a: var name given to input also used in the for loop.
+* void - returns no value, instead the numbers are printed to the screen
+* Return: 0.
+*/
+void print_numbers(void);
+{
+for (int a = 48; a < 58; a++)
+{
+    putchar(a);
+}
+putchar("\n");
+return (0);
+}
