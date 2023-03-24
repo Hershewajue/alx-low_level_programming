297f05fafa24101fe480d358320601e7f6978204
Hershewajue <amedujosepho@gmail.com>
2023-03-24 16:04:36 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*_isdigit - this function checks if the input is a digit
+*@c: an integer
+*Return: 1 if true and 0 if false
+*/
+int mul(int a, int b);
+{
+return (a * b);
+}
