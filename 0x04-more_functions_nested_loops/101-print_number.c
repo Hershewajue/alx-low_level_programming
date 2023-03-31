84e8223b001a3cab0a80c0679a0ccfa2ad21c29b
Hershewajue <amedujosepho@gmail.com>
2023-03-31 12:40:10 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_number - Prints an integer
+ *
+ * @n: The integer to print
+ */
+void print_number(int n)
+{
+if (n < 0) {
+_putchar('-');
+n = -n;
+}
+
+if (n / 10)
+print_number(n / 10);
+
+_putchar(n % 10 + '0');
+}
