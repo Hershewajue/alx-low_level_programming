4a0e953a843952b215d4d8bf701f9c308abce86e
Hershewajue <amedujosepho@gmail.com>
2023-03-31 12:22:22 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 #define PASSWORD_LENGTH 12
 
@@ -14,6 +15,7 @@ int main(void)
     int i;
     char password[PASSWORD_LENGTH + 1];
     const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    const char correct_password[] = "elfhgcwaql";
 
     /* Seed the random number generator with the current time */
     srand(time(NULL));
@@ -28,8 +30,15 @@ int main(void)
     /* Terminate the password string */
     password[PASSWORD_LENGTH] = '\0';
 
-    /* Print the generated password */
-    printf("%s\n", password);
+    /* Check if the generated password matches the correct password */
+    if (strcmp(password, correct_password) == 0)
+    {
+        printf("Tada! Congrats\n");
+    }
+    else
+    {
+        printf("Wrong password\n");
+    }
 
     return (0);
 }
