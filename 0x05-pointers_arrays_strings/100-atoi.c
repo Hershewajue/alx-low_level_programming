19d0cae233adaa3b4d8bf3c8b7b95ff83281da4c
Hershewajue <amedujosepho@gmail.com>
2023-03-31 12:10:19 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,23 +8,12 @@ int _atoi(char *s)
 {
 int result = 0;
 int sign = 1;
-
 while (*s != '\0')
 {
 if (*s == '-')
 sign *= -1;
 else if (*s >= '0' && *s <= '9')
-{
-// Check for overflow before updating result
-if (result > INT_MAX / 10 || (result == INT_MAX / 10 && (*s - '0') > INT_MAX % 10))
-{
-if (sign == 1)
-return (INT_MAX);
-else
-return (INT_MIN);
-}
 result = result * 10 + (*s - '0');
-}
 else if (result > 0)
 break;
 s++;
