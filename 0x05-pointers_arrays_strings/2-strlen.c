d2c6df72cd989c3d03a26544f4473d5a38056366
Hershewajue <amedujosepho@gmail.com>
2023-03-31 11:33:19 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,14 +7,14 @@
  */
 int _strlen(char *s)
 {
-    int len = 0;
+int len = 0;
 
-    while (*s != '\0')
-    {
-        len++;
-        s++;
-    }
+while (*s != '\0')
+{
+len++;
+s++;
+}
 
-    return (len);
+return (len);
 }
 
