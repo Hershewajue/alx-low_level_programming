8b24db631bac1f53d4bb3b47430cac32fcd1bdf9
Hershewajue <amedujosepho@gmail.com>
2023-04-17 15:02:19 -0700

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * Prints the minimum number of coins to make change for an amount of money in cents.
+ *
+ * Usage: ./change cents
+ * where cents is the amount of cents you need to give back
+ *
+ * If the number of arguments passed to the program is not exactly 1,
+ * prints Error followed by a new line and returns 1.
+ * If the number passed as the argument is negative, prints 0 followed by a new line.
+ *
+ * The program uses an unlimited number of coins of values 25, 10, 5, 2, and 1 cent.
+ *
+ * @param argc The number of arguments passed to the program
+ * @param argv An array of strings containing the arguments
+ * @return 0 on success, 1 on error
+ */
+int main(int argc, char **argv)
+{
+int total, count;
+unsigned int i;
+char *p;
+int cents[] = {25, 10, 5, 2};
+
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+
+total = strtol(argv[1], &p, 10);
+count = 0;
+
+if (!*p)
+{
+while (total > 1)
+{
+for (i = 0; i < sizeof(cents[i]); i++)
+{
+if (total >= cents[i])
+{
+count += total / cents[i];
+total = total % cents[i];
+}
+}
+}
+if (total == 1)
+count++;
+}
+else
+{
+printf("Error\n");
+return (1);
+
+}
+
+printf("%d\n", count);
+return (0);
+}
