09470c936d375e3f39a68dd863adc4b408038726
Hershewajue <amedujosepho@gmail.com>
2023-04-17 21:35:46 -0700

    4-sum.h

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,15 @@
+#ifndef 4_SUM_H
+#define 4_SUM_H
+#define SUM_H
+
+/**
+ * SUM - Computes the sum of two numbers x and y
+ * @x: The first number to add
+ * @y: The second number to add
+ *
+ * Return: The sum of x and y
+ */
+#define SUM(x, y) ((x) + (y))
+
+#endif /* SUM_H */
+
