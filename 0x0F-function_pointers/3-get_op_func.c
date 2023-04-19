fd99bd7e7858cdd0ea9e6f5f34841dc0e2268428
Hershewajue <amedujosepho@gmail.com>
2023-04-19 10:41:43 -0700

    some fix

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -11,15 +11,15 @@
 int (*get_op_func(char *s))(int, int)
 {
 op_t ops[] = {
-{&quot;+&quot;, op_add},
-{&quot;-&quot;, op_sub},
-{&quot;*&quot;, op_mul},
-{&quot;/&quot;, op_div},
-{&quot;%&quot;, op_mod},
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
 {NULL, NULL},
 };
 int i = 0;
-while (ops[i].op != NULL &amp;&amp; *(ops[i].op) != *s)
+while (ops[i].op != NULL && *(ops[i].op) != *s)
 i++;
 return (ops[i].f);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,24 +15,24 @@ int num1, num2;
 char *op;
 if (argc != 4)
 {
-printf(&quot;Error\n&quot;);
+printf("Error\n");
 exit(98);
 }
 num1 = atoi(argv[1]);
 op = argv[2];
 num2 = atoi(argv[3]);
-if (get_op_func(op) == NULL || op[1] != &#39;\0&#39;)
+if (get_op_func(op) == NULL || op[1] != '\0')
 {
-printf(&quot;Error\n&quot;);
+printf("Error\n");
 exit(99);
 }
-if ((*op == &#39;/&#39; &amp;&amp; num2 == 0) ||
-(*op == &#39;%&#39; &amp;&amp; num2 == 0))
+if ((*op == '/' && num2 == 0) ||
+(*op == '%' && num2 == 0))
 {
-printf(&quot;Error\n&quot;);
+printf("Error\n");
 exit(100);
 }
 
-printf(&quot;%d\n&quot;, get_op_func(op)(num1, num2));
+printf("%d\n", get_op_func(op)(num1, num2));
 return (0);
 }
