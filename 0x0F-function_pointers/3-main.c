66b6bcdb5f7a28544d8deef1140e4ee173e48ad1
Hershewajue <amedujosepho@gmail.com>
2023-04-19 10:14:24 -0700

    3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include &quot;function_pointers.h&quot;
+#include &lt;stdlib.h&gt;
+#include &lt;stdio.h&gt;
+#include &quot;3-calc.h&quot;
+/**
+* main - Prints the result of simple operations.
+* @argc: The number of arguments supplied to the program.
+* @argv: An array of pointers to the arguments.
+*
+* Return: Always 0.
+*/
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+int num1, num2;
+char *op;
+if (argc != 4)
+{
+printf(&quot;Error\n&quot;);
+exit(98);
+}
+num1 = atoi(argv[1]);
+op = argv[2];
+num2 = atoi(argv[3]);
+if (get_op_func(op) == NULL || op[1] != &#39;\0&#39;)
+{
+printf(&quot;Error\n&quot;);
+exit(99);
+}
+if ((*op == &#39;/&#39; &amp;&amp; num2 == 0) ||
+(*op == &#39;%&#39; &amp;&amp; num2 == 0))
+{
+printf(&quot;Error\n&quot;);
+exit(100);
+}
+
+printf(&quot;%d\n&quot;, get_op_func(op)(num1, num2));
+return (0);
+}
