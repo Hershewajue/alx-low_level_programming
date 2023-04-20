3b8474e69cb2ea164c0de33be6c34825ab8b9ba0
Hershewajue <amedujosepho@gmail.com>
2023-04-20 08:56:23 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -15,14 +15,14 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i = 0;
+	unsigned int i = 0;
 
-while (i < n)
-{
-s[i] = b;
-i++;
-}
-return (s);
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
 }
 
 /**
@@ -37,16 +37,16 @@ return (s);
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-char *ptr;
+	char *ptr;
 
-if (nmemb == 0 || size == 0)
-return (NULL);
-ptr = malloc(nmemb * size);
-if (ptr == NULL)
-return (NULL);
-_memset(ptr, 0, nmemb * size);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	_memset(ptr, 0, nmemb * size);
 
-return (ptr);
+	return (ptr);
 }
 
 
@@ -61,45 +61,45 @@ return (ptr);
 
 void multiply(char *s1, char *s2)
 {
-int i, l1, l2, total_l, f_digit, s_digit, res = 0, tmp;
-char *ptr;
-void *temp;
+	int i, l1, l2, total_l, f_digit, s_digit, res = 0, tmp;
+	char *ptr;
+	void *temp;
 
-l1 = _length(s1);
-l2 = _length(s2);
-tmp = l2;
-total_l = l1 + l2;
-ptr = _calloc(sizeof(int), total_l);
+	l1 = _length(s1);
+	l2 = _length(s2);
+	tmp = l2;
+	total_l = l1 + l2;
+	ptr = _calloc(sizeof(int), total_l);
 
-/* store our pointer address to free later */
-temp = ptr;
+	/* store our pointer address to free later */
+	temp = ptr;
 
-for (l1--; l1 >= 0; l1--)
-{
-f_digit = s1[l1] - '0';
-res = 0;
-l2 = tmp;
-for (l2--; l2 >= 0; l2--)
-{
-s_digit = s2[l2] - '0';
-res += ptr[l2 + l1 + 1] + (f_digit * s_digit);
-ptr[l1 + l2 + 1] = res % 10;
-res /= 10;
-}
-if (res)
-ptr[l1 + l2 + 1] = res % 10;
-}
+	for (l1--; l1 >= 0; l1--)
+	{
+		f_digit = s1[l1] - '0';
+		res = 0;
+		l2 = tmp;
+		for (l2--; l2 >= 0; l2--)
+		{
+			s_digit = s2[l2] - '0';
+			res += ptr[l2 + l1 + 1] + (f_digit * s_digit);
+			ptr[l1 + l2 + 1] = res % 10;
+			res /= 10;
+		}
+		if (res)
+			ptr[l1 + l2 + 1] = res % 10;
+	}
 
-while (*ptr == 0)
-{
-ptr++;
-total_l--;
-}
+	while (*ptr == 0)
+	{
+		ptr++;
+		total_l--;
+	}
 
-for (i = 0; i < total_l; i++)
-printf("%i", ptr[i]);
-printf("\n");
-free(temp);
+	for (i = 0; i < total_l; i++)
+		printf("%i", ptr[i]);
+	printf("\n");
+	free(temp);
 }
 
 
@@ -117,18 +117,18 @@ free(temp);
 
 int main(int argc, char *argv[])
 {
-char *n1 = argv[1];
-char *n2 = argv[2];
+	char *n1 = argv[1];
+	char *n2 = argv[2];
 
-if (argc != 3 || check_number(n1) || check_number(n2))
-error_exit();
+	if (argc != 3 || check_number(n1) || check_number(n2))
+		error_exit();
 
-if (*n1 == '0' || *n2 == '0')
-{
-_putchar('0');
-_putchar('\n');
-}
-else
-multiply(n1, n2);
-return (0);
+	if (*n1 == '0' || *n2 == '0')
+	{
+		_putchar('0');
+		_putchar('\n');
+	}
+	else
+		multiply(n1, n2);
+	return (0);
 }
