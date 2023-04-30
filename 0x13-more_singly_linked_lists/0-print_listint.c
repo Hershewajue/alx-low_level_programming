2df7113d57ff07116f6808e67bd72e56afe647ab
Hershewajue <amedujosepho@gmail.com>
2023-04-30 08:28:39 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * print_listint - a method that prints every item in a list
+ *
+ * @h: pointer to first node
+ *
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+size_t node_counter = 1;
+
+/* if h is null, return 0 as the number of nodes*/
+if (h == NULL)
+return (0);
+
+while (h->next != NULL)
+{
+printf("%d\n", h->n);
+
+/*proceed to the following node*/
+h = h->next;
+
+/*tally the number of nodes*/
+node_counter++;
+}
+
+/*displays the last node*/
+printf("%d\n", h->n);
+
+return (node_count);
+}
