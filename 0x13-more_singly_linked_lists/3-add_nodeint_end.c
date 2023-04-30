37811d82fa53030714fa6b04d0dbf631981c9028
Hershewajue <amedujosepho@gmail.com>
2023-04-30 09:42:52 -0700

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,37 +1,39 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - a function that adds a new node at the end of the node
+ * add_nodeint_end - a function that adds a new node
+ *            at the end of the node
  *
- * @head: the pointer to the first node of the list
- * @n: the value to be added to the new node
+ * @head: pointer to the first node of the list
+ * @n: element int to add to new node
  *
- * Return: either NULL if it succeeds or the address of the new element
+ * Return: address of the new element or NULL if it fails
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-listint_t *new_node, *temp;
+	listint_t *new_node, *temp;
 
-new_node = malloc(sizeof(listint_t));
-if (new_node == NULL)
-return (NULL);
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-new_node->n = n;
-new_node->next = NULL;
+	new_node->n = n;
+	new_node->next = NULL;
 
-temp = *head;
+	temp = *head;
 
-/*add the node, if the head is empty*/
-if (temp == NULL)
-*head = new_node;
+	/*place new_node first if head is NULL*/
+	if (temp == NULL)
+		*head = new_node;
+	else
+	{
+		/*if head is not null, and next is not NULL*/
+		while (temp->next != NULL)
+			temp = temp->next;	/*go to the last node*/
 
-else
-/*check if the node is not empty*/
-while (temp->next != NULL)
-/*while the condition is true, iterate through the loop*/
-temp = temp->next;	
-/*affix the fresh node to the list's end/
-temp->next = new_node;
-}
-return (*head);
+		/*place our new node at the end*/
+		temp->next = new_node;
+	}
+
+	return (*head);
 }
