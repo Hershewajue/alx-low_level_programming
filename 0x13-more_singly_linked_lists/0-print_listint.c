d272d990e8e1b26a837437655559789fbf55c7ba
Hershewajue <amedujosepho@gmail.com>
2023-04-30 08:47:34 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,25 +9,24 @@
  */
 size_t print_listint(const listint_t *h)
 {
-size_t node_count = 1;
+	size_t node_count = 1;
 
-/* if h is null, return 0 as the number of nodes*/
-if (h == NULL)
-return (0);
+	/* if h is null, return 0 as the number of nodes*/
+	if (h == NULL)
+		return (0);
 
-while (h->next != NULL)
-{
-printf("%d\n", h->n);
+	while (h->next != NULL)
+	{
+		printf("%d\n", h->n);
 
-/*proceed to the following node*/
-h = h->next;
+		/*proceed to the following node*/
+		h = h->next;
+		/*tally the number of nodes*/
+		node_count++;
+	}
 
-/*tally the number of nodes*/
-node_count++;
-}
+	/*displays the last node*/
+	printf("%d\n", h->n);
 
-/*displays the last node*/
-printf("%d\n", h->n);
-
-return (node_count);
+	return (node_count);
 }
